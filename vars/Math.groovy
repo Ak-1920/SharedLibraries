70697f8a4defc76a0ae3d401d9c80829e98878cf
Ak-1920 <146092224+Ak-1920@users.noreply.github.com>
2024-03-25 23:14:07 +0530

    Update Math.groovy

--- vars/Math.groovy
+++ vars/Math.groovy
@@ -1,1 +1,13 @@
+def add(a,b)
+{
+  echo "The sum of ${a} and ${b} is ${a+b}"
+}
+def sub(a,b)
+{
+  echo "The difference of ${a} and ${b} is ${a-b}"
+}
+def mul(a,b)
+{
+  echo "The multiplication od ${a} and $b} is ${a*b}"
+}
 
