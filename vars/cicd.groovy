73383ef0b22d402f5a4870b959fdc161d251c7a2
Ak-1920 <146092224+Ak-1920@users.noreply.github.com>
2024-03-27 00:49:30 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -6,7 +6,7 @@ def mavenbuild()
 {
   sh 'mvn package'
 }
-def deploytoQA(jobname,ip,appname)
+def deploy(jobname,ip,appname)
 {
   sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${appname}.war"
 }
