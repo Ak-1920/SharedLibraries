dc071bc407aca314b4315448fffd8afabad0a7da
Ak-1920 <146092224+Ak-1920@users.noreply.github.com>
2024-03-26 21:01:08 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -4,5 +4,14 @@ def gitdownload(repo)
 }
 def mavenbuild()
 {
-  sh "mvn package"
+  sh 'mvn package'
 }
+def deploytoQA(jobname,ip,appname)
+{
+  sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${appname}.war"
+}
+def runSelenium(jobname)
+{
+sh "java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar"
+}
+
